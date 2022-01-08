5e52812262748a23ef05ce851b67bd4483d2d70e
edu-lance <imail.uxiang@icloud.com>
2022-01-08 21:46:49 +0800

    11

--- app/src/main/java/com/enjoy/appstartup/MyApplication.java
+++ app/src/main/java/com/enjoy/appstartup/MyApplication.java
@@ -14,13 +14,20 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        new StartupManager.Builder()
-                .addStartup(new Task5())
-                .addStartup(new Task4())
-                .addStartup(new Task3())
-                .addStartup(new Task2())
-                .addStartup(new Task1())
-                .build(this)
-                .start().await();
+
+//        new Task1().create(MyApplication.this);
+//        new Task2().create(MyApplication.this);
+//        new Task3().create(MyApplication.this);
+//        new Task4().create(MyApplication.this);
+//        new Task5().create(MyApplication.this);
+
+//        new StartupManager.Builder()
+//                .addStartup(new Task5())
+//                .addStartup(new Task4())
+//                .addStartup(new Task3())
+//                .addStartup(new Task2())
+//                .addStartup(new Task1())
+//                .build(this)
+//                .start().await();
     }
 }
