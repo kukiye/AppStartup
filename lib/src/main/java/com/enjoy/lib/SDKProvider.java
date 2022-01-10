617bb1b9e6ad60b9c56534609e1cbf19b7e3f1d2
edu-lance <imail.uxiang@icloud.com>
2022-01-10 16:16:29 +0800

    11

---
+++ lib/src/main/java/com/enjoy/lib/SDKProvider.java
@@ -0,0 +1,50 @@
+package com.enjoy.lib;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SDKProvider extends ContentProvider {
+    private static final String TAG = "SDKProvider";
+
+    @Override
+    public boolean onCreate() {
+        Log.i(TAG, "onCreate: SDK初始化");
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
